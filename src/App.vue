015c0916edd40ca157e8014861174c5d05e82877
abbi363 <abbi.engel@snapit.solutions>
2022-07-01 10:56:15 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<h1>Hello (: </h1>
+</div> 
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+@import "./style.css"
+</style>
