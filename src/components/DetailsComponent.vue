187e8b069639ec27a322ea38b895cb6f19202ea1
Abbi Engel <abbi.engel33@gmail.com>
2022-07-07 16:17:38 -0500

    3

---
+++ src/components/DetailsComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="grid">
+            <div class="row">
+                <div class="col">
+                    <li class="list-name">
+                        <ul class="list-lastName">
+                            firstName: {{name}}
+                        </ul>
+                    </li>
+                </div>
+            </div>
+        </div>
+        <Customer firstName="Abbi" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Team",
+        props: {
+            name: {type: String, required: true}
+        }
+    }
+</script>
+
+
+<style>
+
+</style>
